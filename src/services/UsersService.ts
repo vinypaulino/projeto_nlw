eda4c379222540de7d7e351e1b1e0096b1907fd1
vinypaulino <vinydos3@gmail.com>
2021-04-23 01:25:23 -0300

    feat: four class

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -25,6 +25,14 @@ class UsersService {
 
     return user;
   }
+
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne({
+      email
+    })
+
+    return user;
+  }
 }
 
 export { UsersService }
